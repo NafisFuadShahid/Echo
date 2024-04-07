aa71025997e62a6454ab6ef7b21c2f09371ee9a5
Maroof Ahmed <maroofahmed@iut-dhaka.edu>
2024-04-08 01:45:50 +0600

    created a Parent node called 'root'

--- src/main/java/org/example/media4/Main.java
+++ src/main/java/org/example/media4/Main.java
@@ -2,6 +2,7 @@ package org.example.media4;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -12,9 +13,11 @@ public class Main extends Application {
     @Override
     // New Branch
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("media-player.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        String imagePath = "src/main/resources/org/example/media4/watching.png";
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("media-player.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load());
+        Parent root = FXMLLoader.load(getClass().getResource("media-player.fxml"));
+        Scene scene = new Scene(root);
+        String imagePath = "src/main/resources/org/example/media4/PlayButtonLogo.png";
         Image icon = new Image("file:" + imagePath);
         stage.getIcons().add(icon);
 
