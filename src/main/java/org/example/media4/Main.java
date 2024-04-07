5f46f4f91d12b5d1c6509ee5b48020e7af14400a
NafisFuad106 <107258321+NafisFuad106@users.noreply.github.com>
2024-04-08 00:23:38 +0600

    added logo

--- src/main/java/org/example/media4/Main.java
+++ src/main/java/org/example/media4/Main.java
@@ -3,6 +3,7 @@ package org.example.media4;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,10 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("media-player.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        String imagePath = "src/main/resources/org/example/media4/watching.png";
+        Image icon = new Image("file:" + imagePath);
+        stage.getIcons().add(icon);
+
         stage.setTitle("Echo");
         stage.setScene(scene);
         stage.show();
