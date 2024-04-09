0105fc0130491700ac75976dc3904bf1a8e6b2c9
Maroof Ahmed <maroofahmed@iut-dhaka.edu>
2024-04-10 00:21:13 +0600

    Rename the 'Select Media' to 'Open'

--- src/main/java/org/example/media4/MediaPlayerController.java
+++ src/main/java/org/example/media4/MediaPlayerController.java
@@ -156,48 +156,48 @@ public class MediaPlayerController implements Initializable {
 
     @FXML
     void selectMedia(ActionEvent event) throws IOException {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Select Media");
-        File selectedFile = fileChooser.showOpenDialog(null);
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open");
+            File selectedFile = fileChooser.showOpenDialog(null);
 
 
-        if (selectedFile != null) {
-            String url = selectedFile.toURI().toString();
+            if (selectedFile != null) {
+                String url = selectedFile.toURI().toString();
 
-            media = new Media(url);
-            mediaPlayer = new MediaPlayer(media);
+                media = new Media(url);
+                mediaPlayer = new MediaPlayer(media);
 
-            mediaView.setMediaPlayer(mediaPlayer);
+                mediaView.setMediaPlayer(mediaPlayer);
 
-            // Update media duration and slider max value when media is ready
-            mediaPlayer.setOnReady(() -> {
-                Duration totalDuration = media.getDuration();
-                slider.setMax(totalDuration.toSeconds());
-                lblDuration.setText("Duration: 00 / " + (int) totalDuration.toSeconds());
-            });
+                // Update media duration and slider max value when media is ready
+                mediaPlayer.setOnReady(() -> {
+                    Duration totalDuration = media.getDuration();
+                    slider.setMax(totalDuration.toSeconds());
+                    lblDuration.setText("Duration: 00 / " + (int) totalDuration.toSeconds());
+                });
 
-            // Bind volumeSlider to MediaPlayer volume property
-            mediaPlayer.volumeProperty().bind(volumeSlider.valueProperty().divide(100.0));
+                // Bind volumeSlider to MediaPlayer volume property
+                mediaPlayer.volumeProperty().bind(volumeSlider.valueProperty().divide(100.0));
 
-            // Set the initial volume of the media player
-            volumeSlider.setValue(75);
+                // Set the initial volume of the media player
+                volumeSlider.setValue(75);
 
-            // Initialize volumeSlider with initial volume value of MediaPlayer
-            if (mediaPlayer != null) {
-                double initialVolume = mediaPlayer.getVolume() * 100.0; // Convert volume range (0.0 - 1.0) to (0 - 100)
-                volumeSlider.setValue(initialVolume);
+                // Initialize volumeSlider with initial volume value of MediaPlayer
+                if (mediaPlayer != null) {
+                    double initialVolume = mediaPlayer.getVolume() * 100.0; // Convert volume range (0.0 - 1.0) to (0 - 100)
+                    volumeSlider.setValue(initialVolume);
+                }
+
+                // Add listener to update slider and lblDuration based on currentTime
+                mediaPlayer.currentTimeProperty().addListener((observableValue, oldValue, newValue) -> {
+                    slider.setValue(newValue.toSeconds());
+                    lblDuration.setText("Duration: " + (int) slider.getValue() + " / " + (int) media.getDuration().toSeconds());
+                });
+
+                Scene scene = mediaView.getScene();
+                mediaView.fitWidthProperty().bind(scene.widthProperty());
+                mediaView.fitHeightProperty().bind(scene.heightProperty());
             }
-
-            // Add listener to update slider and lblDuration based on currentTime
-            mediaPlayer.currentTimeProperty().addListener((observableValue, oldValue, newValue) -> {
-                slider.setValue(newValue.toSeconds());
-                lblDuration.setText("Duration: " + (int) slider.getValue() + " / " + (int) media.getDuration().toSeconds());
-            });
-
-            Scene scene = mediaView.getScene();
-            mediaView.fitWidthProperty().bind(scene.widthProperty());
-            mediaView.fitHeightProperty().bind(scene.heightProperty());
-        }
     }
 
     @FXML
